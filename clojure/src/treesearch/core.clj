aa004df52c8a328f23f6b4b0452c203d8b518376
esc <esc@youhavethewrong.info>
2015-01-20 22:16:21 -0600

    defined some strategy tests and made them pass.  working on expansion tests.

--- clojure/src/treesearch/core.clj
+++ clojure/src/treesearch/core.clj
@@ -20,62 +20,78 @@
 ;; discovered) and see how much of the search spaceeach algorithm
 ;; explores.
 
+(defrecord Problem [initial-state goal-state])
+(defrecord Strategy [function])
+(defrecord SearchNode [state parentNode action pathCost treeDepth])
 
-(defn binary-search1
-  [coll ^long coll-size  target]
-  (let [cnt (dec coll-size)]
-    (loop [low-idx 0 high-idx cnt]
-      (if (> low-idx high-idx)
-        nil
-        (let [mid-idx (quot (+ low-idx high-idx) 2) mid-val (coll mid-idx)]
-          (cond
-           (= mid-val target) mid-idx
-           (< mid-val target) (recur (inc mid-idx) high-idx)
-           (> mid-val target) (recur low-idx (dec mid-idx))))))))
+(def actions
+  (list  {:r {:m 1 :c 1}} 
+         {:r {:m 0 :c 2}}
+         {:r {:m 0 :c 1}}
+         {:r {:m 2 :c 0}}
+         {:r {:m 1 :c 0}}
+         {:l {:m 1 :c 1}}
+         {:l {:m 0 :c 2}}
+         {:l {:m 0 :c 1}}
+         {:l {:m 2 :c 0}}
+         {:l {:m 1 :c 0}}))
 
-(defn binary-search2
-  "Finds earliest occurrence of x in xs (a vector) using binary search."
-  ([xs x]
-     (loop [l 0 h (unchecked-dec (count xs))]
-       (if (<= h (inc l))
-         (cond
-          (== x (xs l)) l
-          (== x (xs h)) h
-          :else nil)
-         (let [m (unchecked-add l (bit-shift-right (unchecked-subtract h l) 1))]
-           (if (< (xs m) x)
-             (recur (unchecked-inc m) h)
-             (recur l m)))))))
+(def problem
+  (Problem. {:initial-state {:l {:m 3 :c 3 :b true}
+                             :r {:m 0 :c 0 :b false}}}
+            {:goal-state {:l {:m 0 :c 0 :b false}
+                          :r {:m 3 :c 3 :b true}}}))
 
-(def explored (atom {}))
+(defn lifo [fringe]
+  (last fringe))
 
-(defn strategy
-  "Pick a node to try next."
-  [fringe]
-  {:state (next fringe)})
+(defn fifo [fringe]
+  (first fringe))
 
 (defn goal-test [problem maybe-goal-state]
   (== (:goal-state problem)
       maybe-goal-state))
 
-(defn applicable [action node])
+(defn apply-action [state action]
+  )
 
-(def problem
-  {:initial-state {:left {:m 3 :c 3 :b true}
-                   :right {:m 0 :c 0 :b false}}}
-  {:goal-state {:left {:m 0 :c 0 :b false}
-                :right {:m 3 :c 3 :b true}}}
-  {:applicable applicable}
-  {:actions [:2m :1m1c :2c]})
+(defn test-action [bank state motion]
+  (let [to bank
+        from (cond (= :l bank) :r :else :l)]
+    (and
+     (<= (+ (:m (to state))   (:m motion)) 3)
+     (<= (+ (:c (to state))   (:c motion)) 3)
+     (>  (- (:m (from state)) (:m motion)) 0)
+     (>  (- (:c (from state)) (:c motion)) 0)
+     (and
+      (> (+ (:m  (to state))   (:m motion)) 0)
+      (<= (+ (:c (from state)) (:c motion))
+          (+ (:m (to state)    (:m motion)))))))) 
 
+(defn valid-action? [action node]
+  (let [state (:state node)]
+    (test-action
+     (first (keys action))
+     state
+     (first (vals action)))))
+    
+(defn expand [fringe node]
+  (conj
+   (map
+    #(SearchNode. (:state node) node % (inc (:pathCost node)) (inc (:treeDepth node)))
+    (filter
+     #(valid-action? % node)
+     actions))
+   fringe))
 
 (defn search-tree [problem strategy]
-  ;; what do i need to reset every loop?  fringe and node?
   (loop [fringe (:initial-state problem)]
-    (if (empty? fringe)
+    (if
+        (or (nil? fringe)
+            (empty? fringe))
       nil
-      (let [node (strategy fringe)]
-        (if (goal-test problem (:state node))
-          
-          )
-        )))) 
+      (let [node ((:fuction strategy) fringe)]
+        (cond
+         (nil? node) nil
+         (goal-test problem (:state node)) node
+         :else (recur (expand fringe node)))))))
--- clojure/test/treesearch/core_test.clj
+++ clojure/test/treesearch/core_test.clj
@@ -2,14 +2,48 @@
   (:require [clojure.test :refer :all]
             [treesearch.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def initial-node
+  (->SearchNode {:l {:m 3 :c 3 :b true} :r {:m 0 :c 0 :b false}}
+                nil
+                nil
+                0
+                0))
 
-(deftest game
-  (testing "should plan the cannibals and missionaries problem"
+(def tinyfringe
+  [(->SearchNode {:l {:m 0 :c 1 :b false} :r {:m 3 :c 2 :b true}}
+                nil
+                {:r {:m 1 :c 1}}
+                1
+                1)
+   (->SearchNode {:l {:m 1 :c 1 :b false} :r {:m 2 :c 2 :b true}}
+                nil
+                {:r {:m 0 :c 1}}
+                1
+                1)])
+
+(deftest exerciseStrategy
+  (testing "should exercise a few scenarios for picking nodes from the fringe"
     (is
-     (= 16
-        (count (search-tree
-                problem
-                strategy))))))
+     (= {:l {:m 0 :c 1 :b false} :r {:m 3 :c 2 :b true}}
+        (let [strat (->Strategy fifo)]
+          (:state ((:function strat) tinyfringe))))))
+    (is
+     (= {:l {:m 1 :c 1 :b false} :r {:m 2 :c 2 :b true}}
+        (let [strat (->Strategy lifo)]
+          (:state ((:function strat) tinyfringe))))))
+
+;; (deftest exerciseExpand
+;;   (testing "should expand the available nodes from the initial state"
+;;     (let [fringe (expand [] initial-node)]
+;;       (is
+;;        (= 3
+;;           (count fringe))))))
+
+
+;; (deftest game
+;;   (testing "should plan the cannibals and missionaries problem"
+;;     (is
+;;      (= 11
+;;         (count (search-tree
+;;                 problem
+;;                 strategy))))))
